93fe93ed51ac4ac9bd95f2bd6f92cb00f1eebc9b
iman <imanelamroussi55@gmail.com>
2023-06-26 11:47:31 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,14 @@
 */
 void rev_string(char *s)
 {
-int 1, i;
+int l, i;
 char temp;
-for (1 = 0; s[1] != '\0'; ++1)
+for (l = 0; s[l] != '\0'; ++l)
 ;
-for (i = 0; i < 1 / 2; i++)
+for (i = 0; i < l / 2; i++)
 {
 temp = s[i];
-s[i] = s[1 - 1 - i];
-s[1 - 1 - i] = temp;
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = temp;
 }
 }
