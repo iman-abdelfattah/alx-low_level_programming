ab423f80731c4bfa1342d7dc1c18807c40606913
iman <imanelamroussi55@gmail.com>
2023-07-07 08:23:30 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line
+ * @argc: integer
+ * @argv: list
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
