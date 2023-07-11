87cc6bff3ff9f081eda32b56e465e1cbf378a5fb
iman <imanelamroussi55@gmail.com>
2023-07-11 04:55:54 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *create_array - creates an array of chars, and initializes it with a specific char
+ * @size: size of memory to print 
+ * @c: int
+ * @p: the address of memory to print
+ * Return: nothing
+*/
+char *create_array(unsigned int size, char c)
+{
+unsigned int size;
+char c[];
+int *p = char *malloc(unsigned int size, char c);
+if (*p == NULL)
+{
+printf("failed to allocate memory\n");
+return (1);
+}
+}
