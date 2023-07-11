fca07074962d02f3263598f12cfaad1b5b4c1949
iman <imanelamroussi55@gmail.com>
2023-07-11 08:08:37 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,8 +7,8 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char *p = (char*) malloc(size *sizeof(char));
-if (p == 0 || size ==0)
+char *p = (char *) malloc(size * sizeof(char));
+if (p == 0 || size == 0)
 return (0);
 while (size--)
 p[size] = c;
